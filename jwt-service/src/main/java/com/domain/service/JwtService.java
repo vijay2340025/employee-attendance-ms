c296c3c5fcb591f091cefd1e912fc1aee6275978
vijay <vijay2340025@gmail.com>
2024-03-19 13:34:37 +0530

    add only claims and remove payload

--- jwt-service/src/main/java/com/domain/service/JwtService.java
+++ jwt-service/src/main/java/com/domain/service/JwtService.java
@@ -35,8 +35,7 @@ public class JwtService {
     private String createToken(Map<String, Object> claims, String userName) {
         return Jwts.builder()
                 .setClaims(claims)
-                .setPayload(userName)
-                //.setSubject(userName)
+                .setSubject(userName)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 //.setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30))
                 .signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
