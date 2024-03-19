870ae2388392f8e5f594f02f28a6f4cb513b5709
vijay <vijay2340025@gmail.com>
2024-03-19 13:12:58 +0530

    remove jwt token expire

--- jwt-service/src/main/java/com/domain/service/JwtService.java
+++ jwt-service/src/main/java/com/domain/service/JwtService.java
@@ -35,9 +35,10 @@ public class JwtService {
     private String createToken(Map<String, Object> claims, String userName) {
         return Jwts.builder()
                 .setClaims(claims)
-                .setSubject(userName)
+                .setPayload(userName)
+                //.setSubject(userName)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30))
+                //.setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30))
                 .signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
     }
 
