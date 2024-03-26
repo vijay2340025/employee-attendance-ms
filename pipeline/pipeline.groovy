f1f668d9857c90e66cff9f04fdb1e04bfccb13ab
vijay <vijay2340025@gmail.com>
2024-03-26 23:07:20 +0530

    add basic infra files

--- pipeline/pipeline.groovy
+++ pipeline/pipeline.groovy
@@ -7,5 +7,14 @@ pipeline {
                 git branch: 'main', credentialsId: 'da6f6042-ce09-4a44-9145-357085d53474', url: 'https://github.com/vijay2340025/employee-attendance-ms.git'
             }
         }
+
+        stage('init') {
+            steps {
+                sh 'sed -i \'3s/.*/  access_key="${access_key}"/\' pipeline/infra/ec2_instance.tf\n'
+                sh 'sed -i \'4s/.*/  secret_key="${secret_key}"/\' pipeline/infra/ec2_instance.tf\n'
+                sh 'cat pipeline/infra/ec2_instance.tf'
+                sh 'init terraform pipeline/infra/'
+            }
+        }
     }
 }
