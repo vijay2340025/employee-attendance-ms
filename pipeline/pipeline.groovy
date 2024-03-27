53fcead09d06a967bfefeee705eeeb3f51e21add
vijay <vijay2340025@gmail.com>
2024-03-27 09:18:46 +0530

    fix: parameters added

--- pipeline/pipeline.groovy
+++ pipeline/pipeline.groovy
@@ -3,6 +3,9 @@ pipeline {
 
     stages {
         stage('init') {
+            when {
+                expression { params.initialize == true }
+            }
             steps {
                 sh "sed -i '2s/.*/  default=\"$params.access_key\"/'  pipeline/infra/variables.tf"
                 sh "sed -i '6s/.*/  default=\"$params.secret_key\"/'  pipeline/infra/variables.tf"
@@ -12,18 +15,36 @@ pipeline {
         }
 
         stage('plan') {
+            when {
+                expression { params.state == "start" }
+            }
             steps {
                 sh 'cd pipeline/infra/ && terraform plan -var-file=dev.tfvars'
             }
         }
 
         stage('apply') {
+            when {
+                expression { params.state == "start" }
+            }
             steps {
                 sh 'cd pipeline/infra/ && terraform apply -auto-approve -var-file=dev.tfvars'
             }
         }
 
+        stage('apply') {
+            when {
+                expression { params.state == "stop" }
+            }
+            steps {
+                sh 'cd pipeline/infra/ && terraform destroy'
+            }
+        }
+
         stage('clean') {
+            when {
+                expression { params.state == "stop" }
+            }
             steps {
                 cleanWs()
             }
