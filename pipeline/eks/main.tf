11564403a36b4f3df9490f2c7f978288deabc9db
vijay <vijay2340025@gmail.com>
2024-04-04 16:07:58 +0530

    feat: add resource for nodegroup

--- pipeline/eks/main.tf
+++ pipeline/eks/main.tf
@@ -13,6 +13,32 @@ resource "aws_eks_cluster" "demo" {
       // add id's of subnets
     ]
   }
-
   depends_on = [aws_iam_role.eks-cluster-role]
+}
+
+resource "aws_eks_node_group" "worker-nodes" {
+  cluster_name    = aws_eks_cluster.demo.name
+  node_group_name = "worker-nodes"
+  node_role_arn   = aws_iam_role.eks-nodegrp-role.arn
+
+  subnet_ids = [
+    // add id's of subnets
+  ]
+
+  capacity_type  = "ON_DEMAND"
+  instance_types = ["t2.medium"]
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
+  }
+
+  labels = {
+    node = "kubenode"
+  }
+
+  depends_on = [
+    aws_iam_role.eks-nodegrp-role
+  ]
 }
