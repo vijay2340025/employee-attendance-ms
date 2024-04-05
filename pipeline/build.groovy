cf82531eadb9532e3c3fcf73e0d27ba560e7a8a7
vijay <vijay2340025@gmail.com>
2024-04-05 13:37:38 +0530

    feat: add build statements

--- pipeline/build.groovy
+++ pipeline/build.groovy
@@ -11,8 +11,17 @@ pipeline {
         stage('docker') {
             steps {
                 withDockerRegistry(url: '', credentialsId: 'docker') {
-                    sh 'docker buildx build -t vijay2340025/access-control-service:latest -f "./access-control-api/Dockerfile"'
+                    sh 'cd access-control-api && docker build -t vijay2340025/access-control-api:latest .'
                     sh 'docker push vijay2340025/access-control-service:latest'
+
+                    sh 'cd gateway-server && docker build -t vijay2340025/api-gateway:latest .'
+                    sh 'docker push vijay2340025/api-gateway:latest'
+
+                    sh 'cd report-api && docker build -t vijay2340025/report-api:latest .'
+                    sh 'docker push vijay2340025/report-api:latest'
+
+                    sh 'cd jwt && docker build -t vijay2340025/jwt:latest .'
+                    sh 'docker push vijay2340025/jwt:latest'
                 }
             }
         }
